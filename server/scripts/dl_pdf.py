b627ab028e01281b53bef76de3b21c3ec172799f
Fouad Zairi <fouad.zairi@ext.leroymerlin.com>
2025-04-20 13:00:29 +0200

    config deploy

--- server/scripts/dl_pdf.py
+++ server/scripts/dl_pdf.py
@@ -29,7 +29,10 @@ def download_images_from_urls(urls, output_path):
         url = url.strip()
         print(f"Processing URL: {url}")
         try:
-            response = requests.get(url)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+            }
+            response = requests.get(url, headers=headers)
             if response.status_code == 200:
                 print(f"Successfully fetched HTML content for {url}")
                 html_content = response.text
@@ -37,7 +40,10 @@ def download_images_from_urls(urls, output_path):
                 for image_url in images:
                     try:
                         print(f"Downloading image from {image_url}")
-                        image_response = requests.get(image_url)
+                        headers = {
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+                        }
+                        image_response = requests.get(image_url, headers=headers)
                         if image_response.status_code == 200:
                             image_data = image_response.content
                             if imghdr.what(None, h=image_data) is not None:
